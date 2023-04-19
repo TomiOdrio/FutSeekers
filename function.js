b21c4ed1f1430f28219d201d5746de42b665d503
Tomás Odriozola <tomi.odriozola.2002@gmail.com>
2023-04-19 12:51:09 -0300

    18/4

--- function.js
+++ function.js
@@ -1,3 +1,17 @@
+// Activar Menu Navegacion
+
+let menu=document.getElementById("menu")
+let menu_active=document.getElementById("menu_active")
+let barras=document.getElementById("menu_bars")
+let cross=document.getElementById("menu_cross")
+menu.addEventListener("click",function(){
+    menu_active.classList.toggle("oculto");
+    barras.classList.toggle("oculto");
+    cross.classList.toggle("oculto");
+})
+
+// Imagen Preview
+
 let images=Array.from(document.getElementsByClassName("imgCarousel"))
 let mainPhoto=document.getElementById("mainPhoto")
 
@@ -8,4 +22,14 @@ function updateImage(event){
 
 images.forEach(function(image){
     image.addEventListener("click",updateImage)
+})
+
+// Hacerse Fan
+
+let boton=document.getElementById("boton")
+let form=document.getElementById("suscripcion")
+boton.addEventListener("click",function(){
+    form.classList.toggle("oculto");
+    barras.classList.toggle("oculto");
+    cross.classList.toggle("oculto");
 })
