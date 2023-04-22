1d60bfa9f6dbe4863e39ec9f92e81df587a5dd1c
Tomás Odriozola <tomi.odriozola.2002@gmail.com>
2023-04-21 23:10:59 -0300

    styles

--- function.js
+++ function.js
@@ -1,35 +1,39 @@
-// Activar Menu Navegacion
-
-let menu=document.getElementById("menu")
-let menu_active=document.getElementById("menu_active")
-let barras=document.getElementById("menu_bars")
-let cross=document.getElementById("menu_cross")
-menu.addEventListener("click",function(){
-    menu_active.classList.toggle("oculto");
-    barras.classList.toggle("oculto");
-    cross.classList.toggle("oculto");
-})
-
 // Imagen Preview
 
-let images=Array.from(document.getElementsByClassName("imgCarousel"))
-let mainPhoto=document.getElementById("mainPhoto")
+let images=Array.from(document.getElementsByClassName("imgCarousel"));
+let mainPhoto=document.getElementById("mainPhoto");
+let iframe=document.getElementById("tabla_posiciones");
+let copa=document.getElementById("tabla_copa");
 
 function updateImage(event){
-    let image=event.target
-    mainPhoto.src=image.src
+    let image=event.target;
+    mainPhoto.src=image.src;
 }
 
 images.forEach(function(image){
-    image.addEventListener("click",updateImage)
+    image.addEventListener("click",updateImage);
+    // image[0].addEventListener("click",function(){
+    //     iframe.style.display="none";
+    //     copa.style.display="block";
+    // })
+    // image[1].addEventListener("click",function(){
+    //     copa.style.display="none";
+    //     iframe.style.display="block";
+    // })
 })
 
 // Hacerse Fan
 
-let boton=document.getElementById("boton")
-let form=document.getElementById("suscripcion")
+let boton=document.getElementById("btn_suscripcion");
+let form=document.getElementById("borroso");
+let cross=document.getElementById("cross");
+let submit=document.getElementById("submit_btn");
 boton.addEventListener("click",function(){
     form.classList.toggle("oculto");
-    barras.classList.toggle("oculto");
-    cross.classList.toggle("oculto");
+})
+cross.addEventListener("click",function(){
+    form.classList.toggle("oculto");
+})
+submit.addEventListener("click",function(){
+    form.classList.toggle("oculto");
 })
